30baca4e56bc0e633f473d006c648f6341e6106e
Paulius Jarosius <jarosius@live.com>
2017-04-11 12:39:25 +0300

    Lettell tool initial commit

---
+++ lib/lettell_view_tool.rb
@@ -0,0 +1,6 @@
+require "lettell_view_tool/version"
+require "lettell_view_tool/renderer"
+
+module LettellViewTool
+  # Your code goes here...
+end
---
+++ lib/lettell_view_tool/version.rb
@@ -0,0 +1,3 @@
+module LettellViewTool
+  VERSION = "0.1.0"
+end
